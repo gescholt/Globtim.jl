f9881efeea4732c2f8f7a0df438e4e871f576234
ghs <scholtengeorgy@gmail.com>
2024-09-22 14:37:26 +0200

    more exports, argument for doc generation

--- src/Globtim.jl
+++ src/Globtim.jl
@@ -1,6 +1,19 @@
 module Globtim
 
-export MainGenerate, ApproxPoly, main_nd, process_output_file, parse_point, camel, CrossInTray, Deuflhard, noisy_Deuflhard
+# Use the README as the module docs
+@doc let
+    path = joinpath(dirname(@__DIR__), "README.md")
+    include_dependency(path)
+    read(path, String)
+end Agents
+
+
+# Exported functions and variables
+export MainGenerate, ApproxPoly, main_nd, process_output_file, parse_point, camel, CrossInTray, Deuflhard, noisy_Deuflhard,
+    random_noise, bivariate_gaussian_noise, tref, Ackley, camel_3, camel, shubert, dejong5, easom, init_gaussian_params,
+    rand_gaussian, HolderTable, CrossInTray, Deuflhard, noisy_Deuflhard, old_alpine1, shubert_4d, camel_4d, camel_3_by_3,
+    cosine_mixture, camel_3_6d, Csendes, alpine1, alpine2
+
 
 using CSV
 using DataFrames
@@ -9,7 +22,7 @@ using LinearSolve
 using LinearAlgebra
 using Distributions
 
-include("lib_func.jl")
+include("lib_func.jl") #list of test functions. 
 include("Samples.jl")
 include("ApproxConstruct.jl")
 include("main_gen.jl")
