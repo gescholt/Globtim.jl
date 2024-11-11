81c81cfebf7243441292d113fc6d32e8395c3847
georgy scholten <scholtengeorgy@gmail.com>
2024-11-11 12:13:52 +0100

    wild stuff

--- src/Globtim.jl
+++ src/Globtim.jl
@@ -12,9 +12,9 @@ end Agents
 export MainGenerate, ApproxPoly, test_input, main_nd, process_output_file, parse_point, camel, CrossInTray, Deuflhard, noisy_Deuflhard,
     random_noise, bivariate_gaussian_noise, tref, Ackley, camel_3, camel, shubert, dejong5, easom, init_gaussian_params,
     rand_gaussian, HolderTable, CrossInTray, Deuflhard, noisy_Deuflhard, old_alpine1, shubert_4d, camel_4d, camel_3_by_3,
-    cosine_mixture, camel_3_6d, Csendes, alpine1, alpine2, create_test_input, Constructor, SupportGen, ChebyshevPolyExact, GaussianParams
-    
-# , LegendrePoly
+    cosine_mixture, camel_3_6d, Csendes, alpine1, alpine2, create_test_input, Constructor, SupportGen, ChebyshevPolyExact,
+    GaussianParams, uniform_grid, lambda_vandermonde, 
+    symbolic_legendre, evaluate_legendre
 
 
 using CSV
@@ -24,9 +24,12 @@ using LinearSolve
 using LinearAlgebra
 using Distributions
 
+
 include("lib_func.jl") #list of test functions. 
 include("Samples.jl")
 include("ApproxConstruct.jl")
 include("main_gen.jl")
+include("SymbolicLegendre.jl")
+# include("legendre_poly.jl")
 
 end
