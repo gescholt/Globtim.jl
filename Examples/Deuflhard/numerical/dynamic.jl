8faf7772c0a9ba0e8285fadab2585774411164e9
georgy scholten <scholtengeorgy@gmail.com>
2024-11-11 15:31:34 +0100

    maybe turn off tests for the time being ?

---
+++ Examples/Deuflhard/numerical/dynamic.jl
@@ -0,0 +1,15 @@
+using Pkg
+Pkg.activate(".")
+using Globtim
+# include("../../../src/lib_func.jl") # Include the library of functions
+
+# Constants and Parameters
+d = 2 # Initial Degree 
+const n, a, b = 2, 1, 1 
+const scale_factor = a / b       # Scaling factor appears in `main_computation`, maybe it should be a parameter.
+const delta, alpha = .5 , 1 / 10  # Sampling parameters
+const tol_l2 = 1.5e-3             # Define the tolerance for the L2-norm
+
+f = Deuflhard
+
+poly_approx = MainGenerate(f, 2, d, delta, alpha, scale_factor, 0.2, verbose=1, basis=:legendre)
