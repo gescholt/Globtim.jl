ce617a1acd30ba6078475acc6c838c29313b0e85
Georgy <scholtengeorgy@gmail.com>
2024-12-31 15:16:14 -0500

    cleaned dependencies Manifest

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,37 +1,67 @@
-include("setup.jl")
-@info "Lotka-Volterra Tests"
+using Test
+using Globtim
+using CSV
+using DataFrames
+using DynamicPolynomials
+using HomotopyContinuation
 
-"""
-We need to work in high precision to avoid numerical errors --> bigint = true
-[]: Need to add the msolve polynomial solver, may be quicker than Homotopy solver
-[]: Need to parallelize this.
-[]: Return the center of the cel in which the critical point was computed. 
-"""
+# Constants and Parameters
+const n, a, b = 2, 5, 1
+const scale_factor = a / b 
+center = [0.0, 0.0]
 
-# First define the model and outputs
-model, params, states, outputs = define_lotka_volterra_model()
-Err_dist = make_error_distance(model, outputs, P_TRUE)
-# Error_distance([0.1, 0.22, 0.3], measured_data=outputs)
+f = camel # Objective function
+d = 6 # Initial Degree 
 
-# Then define Error_distance with captured model and outputs
-results_1 = run_parameter_sweep(Err_dist, 4, model, outputs, sample_configs_1, method="msolve");
-# results_2 = run_parameter_sweep(Err_dist, 4, model, outputs, sample_configs_2, method="msolve");
-# results_2 = run_parameter_sweep(Error_distance, model, outputs, sample_configs_2);
-# results_3 = run_parameter_sweep(Error_distance, model, outputs, sample_configs_3);
-# results_4 = run_parameter_sweep(Error_distance, model, outputs, sample_configs_4);
-# results_5 = run_parameter_sweep(Error_distance, model, outputs, sample_configs_5);
+# Load the pre-computed critical points of the camel function
+camel_file_path = "../data/camel_d6.csv"
+# Read the CSV file and convert it to a DataFrame
+camel_df = DataFrame(CSV.File(camel_file_path))
 
-println(results_1)
+const eps = 1e-6 # Define the tolerance for the critical points
+# Function to print a check mark and a message
+function print_success_message(test_name)
+    println("✔️ Critical point captured at tolerance: $test_name")
+end
 
-# To analyze all results:
-# for (i, res) in enumerate(results_3)
-#     println("\n=== Configuration $i ===")
-#     println("Grid points (GN): $(sample_configs[i][2])")  # Get GN from sample_configs
-#     println("Sample range: $(res.sample_range)")
-#     println("Degree of approximant: $(res.degree)")
-#     println("Number of critical points of approximant: $(nrow(res.result.dataframe))")
-#     df = res.result.dataframe
-#     println("\nSorted dataframe by eval_distance:")
-#     println(first(df, 5))  # Only print first 10 rows
-#     println("\n" * "="^50)  # Separator line
-# end
+
+TR = test_input(f,
+    dim=n,
+    center=center,
+    GN=60,
+    sample_range=scale_factor
+)
+pol_cheb = Constructor(TR, d, basis=:chebyshev)
+pol_lege = Constructor(TR, d, basis=:legendre)
+
+@polyvar(x[1:n]) # Define polynomial ring 
+# Chebyshev 
+real_pts_cheb = solve_polynomial_system(x, n, d, pol_cheb.coeffs; basis=:chebyshev, bigint=true)
+df_cheb = process_critical_points(real_pts_cheb, f, scale_factor)
+# Legendre
+real_pts_lege = solve_polynomial_system(x, n, d, pol_lege.coeffs; basis=:legendre, bigint=true)
+df_lege = process_critical_points(real_pts_lege, f, scale_factor)
+
+@testset "Globtim Tests" begin
+    @test camel([3.0, 3.0]) == 405.9
+    # Add more tests as needed
+
+    # Test the camel function
+    for x_known in eachrow(camel_df)
+        x0 = [x_known.x, x_known.y]
+        distances_cheb = [norm(x0 - [row2.x1, row2.x2]) for row2 in eachrow(df_cheb)]
+        distances_lege = [norm(x0 - [row2.x1, row2.x2]) for row2 in eachrow(df_lege)]
+        # Check the result using the `Test.Pass` type
+        if minimum(distances_cheb) < eps
+            @test true
+            print_success_message(eps)
+        end
+        if minimum(distances_lege) < eps
+            @test true
+            print_success_message(eps)
+        else
+            @test false
+        end
+    end
+
+end
